7c295264f03199cb8100f689a846f3e48aec1fa0
elahe karamzade <eli.ka1991@gmail.com>
2022-05-22 14:14:32 +0430

    Updates

---
+++ precache-manifest.2927684060990a6680552f8f3715f35b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1007bd504c67c053564e85f7ed828c9a",
+    "url": "/maktabkhoone-resume/index.html"
+  },
+  {
+    "revision": "29e9ff64b1c4804320ac",
+    "url": "/maktabkhoone-resume/static/css/main.7bcd2695.chunk.css"
+  },
+  {
+    "revision": "e3108a6de7acbdf0d00b",
+    "url": "/maktabkhoone-resume/static/js/2.0f2f42ca.chunk.js"
+  },
+  {
+    "revision": "29e9ff64b1c4804320ac",
+    "url": "/maktabkhoone-resume/static/js/main.2dc4d693.chunk.js"
+  },
+  {
+    "revision": "bded51633f9f67916b3c",
+    "url": "/maktabkhoone-resume/static/js/runtime-main.ac01217c.js"
+  }
+]);
