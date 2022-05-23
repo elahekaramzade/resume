443020cae8c1a1fad219a9e4bf91e9bc979a68ae
elahe karamzade <eli.ka1991@gmail.com>
2022-05-23 21:50:14 +0430

    more details

--- src/sections/AboutSection.jsx
+++ src/sections/AboutSection.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import Fullpage from "../component/Fullpage";
 import data from "../data";
-import {  Element } from "react-scroll";
+import { Element } from "react-scroll";
 
 const AboutSection = () => {
   return (
     <Element name="About" className="element">
-      <Fullpage className="second">
-      <h3>{data.sections[0].title}</h3>
-      <p>{data.sections[0].items[0].content}</p>
-    </Fullpage>
+      <Fullpage className="second about-me">
+        <h3>{data.sections[0].title}</h3>
+        <div>
+          <p>{data.sections[0].items[0].content}</p>
+        </div>
+      </Fullpage>
     </Element>
 
-    
+
   );
 };
 export default AboutSection;
